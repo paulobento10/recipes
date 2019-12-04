1b85e234ae9063eaa55137e6e5abbcb8c77aab5a
Paulo Bento <paulobento1997@gmail.com>
2019-12-04 16:08:11 +0000

    Docker for Golang(no Gin) and postgres working

---
+++ go/app/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	/*"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	"github.com/json-iterator/go"
+	_ "github.com/lib/pq"*/
+)
+
+//Variaveis fixas de postgresBD
+const (
+	dbHost     = "postgres"
+	dbUser     = "docker"
+	dbPassword = "docker"
+	dbName     = "recipes"
+)
+
+func main() {
+
+}
