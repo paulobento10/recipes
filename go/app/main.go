88b801ef432ff93283a1b1eeac9ef3bbc87ce6e9
Paulo Bento <paulobento1997@gmail.com>
2019-12-05 17:05:36 +0000

    Changes to DB schema

--- go/app/main.go
+++ go/app/main.go
@@ -10,37 +10,101 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
-	"github.com/json-iterator/go"
-	_ "github.com/lib/pq"*/)
+	"github.com/json-iterator/go"*/
+	_ "github.com/lib/pq")
+
+	/*------------SQL DA BD------------------
+	CREATE TABLE Users
+	(
+	  user_id INT NOT NULL,
+	  user_name VARCHAR(50) NOT NULL,
+	  password VARCHAR NOT NULL,
+	  PRIMARY KEY (user_id),
+	  UNIQUE (user_name)
+	);
+
+	CREATE TABLE Recipes
+	(
+	  recipe_id INT NOT NULL,
+	  recipe_name VARCHAR(50) NOT NULL,
+	  recipe_description VARCHAR(500) NOT NULL,
+	  PRIMARY KEY (recipe_id)
+	);
+
+	CREATE TABLE Ingredients
+	(
+	  ingredient_id INT NOT NULL,
+	  ingredient_name VARCHAR(50) NOT NULL,
+	  PRIMARY KEY (ingredient_id),
+	  UNIQUE (ingredient_name)
+	);
+
+	CREATE TABLE Directions
+	(
+	  direction_id INT NOT NULL,
+	  direction_details VARCHAR(500) NOT NULL,
+	  direction_order INT NOT NULL,
+	  recipe_id INT NOT NULL,
+	  PRIMARY KEY (direction_id),
+	  FOREIGN KEY (recipe_id) REFERENCES Recipes(recipe_id)
+	);
+
+	CREATE TABLE UsersRecipes
+	(
+	  user_recipe_id INT NOT NULL,
+	  user_id INT NOT NULL,
+	  recipe_id INT NOT NULL,
+	  PRIMARY KEY (user_recipe_id),
+	  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	  FOREIGN KEY (recipe_id) REFERENCES Recipes(recipe_id)
+	);
+
+	CREATE TABLE RecipeIngredients
+	(
+	  recipeIngredient_id INT NOT NULL,
+	  ingredient_id INT NOT NULL,
+	  recipe_id INT NOT NULL,
+	  PRIMARY KEY (recipeIngredient_id),
+	  FOREIGN KEY (ingredient_id) REFERENCES Ingredients(ingredient_id),
+	  FOREIGN KEY (recipe_id) REFERENCES Recipes(recipe_id)
+	);
+	------------SQL DA BD------------------*/
 
 type Users struct {
-	Id       int    `json:"id"`
-	Name     string `json:"name"`
+	User_id       int    `json:"user_id"`
+	User_name     string `json:"user_name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
 type Recipes struct {
-	Id      int    `json:"id"`
-	User_id int    `json:"user_id"`
-	Name    string `json:"name"`
-}
-
-type IngredientsList struct {
-	Recipe_id      int `json:"recipe_id"`
-	Ingredient_id  int `json:"ingredient_id"`
-	Ingredient_qty int `json:"recipe_qty"`
+	Recipe_id      int    `json:"recipe_id"`
+	Recipe_name    string `json:"recipe_name"`
+	Recipe_description    string `json:"recipe_description"`
 }
 
 type Ingredients struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	Ingredient_id   int    `json:"ingredient_id"`
+	Ingredient_name string `json:"ingredient_name"`
 }
 
-type DirectionsList struct {
-	Recipe_id        int    `json:"recipe_id"`
-	Direction_text   string `json:"direction_text"`
-	Direction_number int    `json:"direction_number"`
+type Directions struct {
+	Direction_id        int    `json:"direction_id"`
+	Direction_details   string `json:"direction_details"`
+	Direction_order int    `json:"direction_number"`
+	Recipe_id int `json:"recipe_id"`
+}
+
+type UsersRecipes struct {
+	User_recipe_id      int    `json:"user_recipe_id"`
+	User_id      int    `json:"user_id"`
+	Recipe_id    int `json:"recipe_id"`
+}
+
+type RecipeIngredients struct {
+	Recipe_ingredient_id      int `json:"recipe_ingredient_id"`
+	Ingredient_id  int `json:"ingredient_id"`
+	Recipe_id int `json:"recipe_id"`
 }
 
 //Variaveis fixas de postgresBD
@@ -52,8 +116,5 @@ const (
 )
 
 func main() {
-	var i Ingredients
-	i.Id = 1
-	i.Name = "Paulo"
-	fmt.Println("O nome deste objeto é: "+i.Name+" e o ID: %d", i.Id)
+	fmt.Println("Test")
 }
