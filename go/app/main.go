e0e16256979fa0c14d5ddd25261e08738b4921c5
Paulo <paulobento1997@gmail.com>
2019-12-05 00:17:36 +0000

    Initial commit, of the schema of the DB

--- go/app/main.go
+++ go/app/main.go
@@ -11,8 +11,37 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
 	"github.com/json-iterator/go"
-	_ "github.com/lib/pq"*/
-)
+	_ "github.com/lib/pq"*/)
+
+type Users struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Recipes struct {
+	Id      int    `json:"id"`
+	User_id int    `json:"user_id"`
+	Name    string `json:"name"`
+}
+
+type IngredientsList struct {
+	Recipe_id      int `json:"recipe_id"`
+	Ingredient_id  int `json:"ingredient_id"`
+	Ingredient_qty int `json:"recipe_qty"`
+}
+
+type Ingredients struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type DirectionsList struct {
+	Recipe_id        int    `json:"recipe_id"`
+	Direction_text   string `json:"direction_text"`
+	Direction_number int    `json:"direction_number"`
+}
 
 //Variaveis fixas de postgresBD
 const (
@@ -23,5 +52,8 @@ const (
 )
 
 func main() {
-	fmt.Println("O joao vai ser vegan em 2020.")
+	var i Ingredients
+	i.Id = 1
+	i.Name = "Paulo"
+	fmt.Println("O nome deste objeto é: "+i.Name+" e o ID: %d", i.Id)
 }
