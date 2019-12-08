efa4941fc5014f980869fc47f1e1f3e5bbdbaa1d
JoaoCoelho8 <33787@ufp.edu.pt>
2019-12-08 17:14:50 +0000

    React Project start / Tests in SignIn and SignUp pages

--- react/react-app/src/App.js
+++ react/react-app/src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { /*Component,*/ useState } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch,Route} from "react-router-dom";
+import { AuthContext } from "./context/auth";
+import PrivateRoute from './PrivateRoute';
+import Show from './Components/Show';
+import Insert from './Components/Insert';
+import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+function App(props) {
+  const [authTokens, setAuthTokens] = useState();
+  
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
   }
+
+  return (
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" component={Show}/>
+              <Route exact path="/signin" component={SignIn}/>
+              <Route exact path="/signup" component={SignUp}/>
+              <PrivateRoute exact path="/insert" component={Insert}/>
+            </Switch>
+          </div>
+          </Router>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
