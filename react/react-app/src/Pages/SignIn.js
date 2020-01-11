dc8c5ad644f96a7579c34f0e10395e8a13a761f7
Paulo <paulobento1997@gmail.com>
2020-01-11 20:22:12 +0000

    Experimental, but working login page, with class

--- react/react-app/src/Pages/SignIn.js
+++ react/react-app/src/Pages/SignIn.js
@@ -1,175 +1,74 @@
-import React, { useState } from "react";
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import axios from 'axios';
-import { useAuth } from "../context/auth";
-import Copyright from "../Components/Copyright";
-//import Redirect from "react-router-dom";
-//import FormControlLabel from '@material-ui/core/FormControlLabel';
-//import Checkbox from '@material-ui/core/Checkbox';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+const axios = require('axios');
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://www.evasoes.pt/files/2017/07/Moura-1200x800.jpg)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+class Login extends Component {
 
-function SignIn(props) {
-  
-  const [isLoggedIn, setLoggedIn] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { setAuthTokens } = useAuth();
-  //const referer = props.location.state.referer || '/';
-
-  function hashPass(pass) {
-    var bcrypt = require('bcryptjs');
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(pass, salt);
-    setPassword(hash);
+  constructor(props){
+    super(props);
+    this.state = {
+      toShow: false,
+    };
+    this.handleLogin=this.handleLogin.bind(this);
   }
-  
-  function postLogin() {
-    var user = {
-      email: email,
-      password: password,
+  handleLogin(e)
+  {
+    e.preventDefault();
+    if(this.refs.email.value === ""){
+      alert("Email is required");
     }
-    console.log(user);
-
-    axios.post("http://localhost:8000/api/login", user)
-    .then(result => {  
-      if (result.data>=0) {
-        console.log(result);
-        setAuthTokens(result.data);
-        setLoggedIn(true);
-        props.history.push('/show');
-      } else {
-        setIsError(true);
+    else if(this.refs.password.value === "") {
+      alert("Password is required");
+    }
+    else{
+      var user = {
+        email: this.refs.email.value,
+        password: this.refs.password.value,
       }
-    }).catch(e => {
-      setIsError(true);
-    });
+      console.log(user);
+      axios.post('http://localhost:8000/api/login', user)
+      .then(response => {
+        console.log(response.data);
+        if (response.data === -1)
+        {
+          alert("Password or username wrong !");
+        }
+        else if (response.data !== -1){// if (response.data.access_token){
+          sessionStorage.setItem('access_token', response.data.access_token);
+          this.setState({toShow: true});
+        }
+      })
+      .catch(error => {
+        alert(error);
+      });
+    }
   }
+  render() {
+    if (this.state.toShow === true) {
+      console.log('entrei')
+      return <Redirect to='/show' />
+    }
 
-  if (isLoggedIn) {
-    props.history.push("/");
+    return (
+      <div className="login">
+      <h3>Login</h3>
+      <form onSubmit={this.handleLogin}>
+      <div className="login-form">
+      <div className="control-group">
+      <input type="text" className="logininput" ref="email" placeholder="Email" />
+      <label className="login-field-icon fui-user" for="login-name"></label>
+      </div>
+      <div className="control-group">
+      <input type="password" class="logininput" ref="password" placeholder="password"/>
+      <label className="login-field-icon fui-lock" for="login-pass"></label>
+      </div>
+      <input className="btn btn-primary btn-large btn-block" type="submit" value="Login" />
+      </div>
+      </form>
+      </div>
+
+    );
   }
-
-  const classes = useStyles();
-
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              onChange={e => {
-                setEmail(e.target.value);
-              }}
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              onChange={e => {
-                setPassword(e.target.value);        //hashPass(e.target.value);
-              }}
-              autoComplete="current-password"
-            />
-            {/*}
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={postLogin}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-            {/*}
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-            */}
-              <Grid item>
-              <Link href="/signup">Don't have an account?</Link>
-                { isError &&<Error>The username or password provided were incorrect!</Error> }
-              </Grid>
-            </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-          </form>
-        </div>
-      </Grid>
-    </Grid>
-  );
 }
 
-export default SignIn;
+export default Login;
