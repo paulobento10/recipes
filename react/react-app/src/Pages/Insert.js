0a05a56a37d496489311eede7e7d9da0755f2b01
JoaoCoelho8 <33787@ufp.edu.pt>
2020-01-13 02:04:26 +0000

    Some changes so user need to be logged in to create recipes

--- react/react-app/src/Pages/Insert.js
+++ react/react-app/src/Pages/Insert.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { useAuth } from "../context/auth";
 import Button from '@material-ui/core/Button';
 
-function Insert(props) {
-  const { setAuthTokens } = useAuth();
+class Insert extends Component {
 
-  function logOut() {
-    setAuthTokens();
+  render() {
+    if (sessionStorage.getItem('access_token') === null) {
+      return <Redirect to='/signin'/>
+    }
+    
+    return (
+      <div>
+        <div>Insert Page - Teste</div>
+        <Button onClick={localStorage.clear()}>Log out</Button>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <div>Insert Page - Teste</div>
-      <Button onClick={logOut}>Log out</Button>
-    </div>
-  );
 }
 
 export default Insert;
