ca127f42cf23afdcb1bedbcacbc0f649b5663a46
JoaoCoelho8 <33787@ufp.edu.pt>
2020-01-13 02:40:08 +0000

    Sshow recipe initialization

--- react/react-app/src/Pages/Recipe.js
+++ react/react-app/src/Pages/Recipe.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
-function Recipe(props) {
+class Recipe extends Component {
 
-  const id = props.match.params.id;
+  constructor(props){
+    super(props);
+    this.state = {
+      id: this.props.match.params.id,
+      isError: false,
+      recipe: [],
+    };
+  }
 
-  return (
-    <div>
-      <h3>Show recipe test!</h3>
-      <h3>Recipe ID: {id}</h3>
-    </div>
-  );
+  componentDidMount() {
+    axios.get("http://localhost:8000/api/searchRecipe/id/"+this.state.id)
+    .then(resulti => {
+      if (resulti.status==200) { 
+        this.setState({recipe: resulti.data[0]});
+      } 
+      if(resulti.data.length<1) {
+        alert("Error!");
+        this.setState({isError: true})
+      }
+    });
+  }
+
+  render() {
+    if (this.state.isError === true) {
+      return <Redirect to='/show'/>
+    }
+    
+    
+    return (
+      <div>
+        <h3>Show recipe test!</h3>
+        <h3>Recipe ID: {this.state.recipe.recipe_id}</h3>
+        <h3>Recipe Name: {this.state.recipe.recipe_name}</h3>
+      </div>
+    );
+  }
 }
 
 export default Recipe;
