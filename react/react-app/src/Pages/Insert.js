f64e664d5b67ea162eb54bcee3eb2c00472e3e22
Paulo <paulobento1997@gmail.com>
2020-01-19 16:58:16 +0000

    Beginig stages of the development of the insert page

--- react/react-app/src/Pages/Insert.js
+++ react/react-app/src/Pages/Insert.js
@@ -1,7 +1,157 @@
 import React, { Component } from 'react';
+import { createMuiTheme, ThemeProvider, withStyles } from '@material-ui/core/styles';
 import { Redirect } from 'react-router-dom';
-import { useAuth } from "../context/auth";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Search from '../Components/Show/ShowSearch';
+import Header from '../Components/Header';
+import Copyright from "../Components/Copyright";
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios'; 
+
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#63ccff',
+      main: '#009be5',
+      dark: '#006db3',
+    },
+  },
+  typography: {
+    h5: {
+      fontWeight: 500,
+      fontSize: 26,
+      letterSpacing: 0.5,
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  props: {
+    MuiTab: {
+      disableRipple: true,
+    },
+  },
+  mixins: {
+    toolbar: {
+      minHeight: 48,
+    },
+  },
+});
+
+theme = {
+  ...theme,
+  overrides: {
+    MuiDrawer: {
+      paper: {
+        backgroundColor: '#18202c',
+      },
+    },
+    MuiButton: {
+      label: {
+        textTransform: 'none',
+      },
+      contained: {
+        boxShadow: 'none',
+        '&:active': {
+          boxShadow: 'none',
+        },
+      },
+    },
+    MuiTabs: {
+      root: {
+        marginLeft: theme.spacing(1),
+      },
+      indicator: {
+        height: 3,
+        borderTopLeftRadius: 3,
+        borderTopRightRadius: 3,
+        backgroundColor: theme.palette.common.white,
+      },
+    },
+    MuiTab: {
+      root: {
+        textTransform: 'none',
+        margin: '0 16px',
+        minWidth: 0,
+        padding: 0,
+        [theme.breakpoints.up('md')]: {
+          padding: 0,
+          minWidth: 0,
+        },
+      },
+    },
+    MuiIconButton: {
+      root: {
+        padding: theme.spacing(1),
+      },
+    },
+    MuiTooltip: {
+      tooltip: {
+        borderRadius: 4,
+      },
+    },
+    MuiDivider: {
+      root: {
+        backgroundColor: '#404854',
+      },
+    },
+    MuiListItemText: {
+      primary: {
+        fontWeight: theme.typography.fontWeightMedium,
+      },
+    },
+    MuiListItemIcon: {
+      root: {
+        color: 'inherit',
+        marginRight: 0,
+        '& svg': {
+          fontSize: 20,
+        },
+      },
+    },
+    MuiAvatar: {
+      root: {
+        width: 32,
+        height: 32,
+      },
+    },
+  },
+};
+
+const styles = {
+  root: {
+    display: 'flex',
+    minHeight: '100vh',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: 256,
+      flexShrink: 0,
+    },
+  },
+  app: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  main: {
+    flex: 1,
+    padding: theme.spacing(6, 4),
+    background: '#eaeff1',
+  },
+  footer: {
+    padding: theme.spacing(2),
+    background: '#eaeff1',
+  },
+};
 
 class Insert extends Component {
 
@@ -9,6 +159,13 @@ class Insert extends Component {
     super(props);
     this.state = {
       toSignIn: false,
+      recipe_name: "",
+      recipe_description:"",
+      user_id:"",
+      duration:"",
+      picture:"",
+      category:"",
+      kcal:"",
     };
     this.handleLogOut=this.handleLogOut.bind(this);
   }
@@ -20,15 +177,86 @@ class Insert extends Component {
   }
 
   render() {
+    const { classes } = this.props;
     if (this.state.toSignIn === true) {
       return <Redirect to='/signin'/>
     }
-    
     return (
-      <div>
-        <div>Insert Page - Teste</div>
-        <Button onClick={this.handleLogOut}>Log out</Button>
-      </div>
+      <ThemeProvider theme={theme}>
+        <Grid container component="main" className={classes.root}>
+          <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+              <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Insert Recipe
+                </Typography>
+                <form className={classes.form} noValidate>
+                  <TextField
+                    ref="recipe_name"
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="recipe_name"
+                    label="recipe_name Address"
+                    name="recipe_name"
+                    autoComplete="recipe_name"
+                    autoFocus
+                    onChange={e => {
+                      this.setState({
+                        recipe_name: e.target.value
+                      });  
+                    }}
+                  />
+                  <TextField
+                    ref="password"
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onChange={e => {
+                      this.setState({
+                        password: e.target.value
+                      });  
+                    }}
+                  />
+                  {/*}
+                  <FormControlLabel
+                    control={<Checkbox value="remember" color="primary" />}
+                    label="Remember me"
+                  />
+                  */}
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    onClick={this.handleLogin}
+                  >
+                    Sign In
+                  </Button>
+                  <Grid container>
+                    <Grid item>
+                      <Link href="/signup">Don't have an account? Sign Up</Link>
+                    </Grid>
+                  </Grid>
+                  <Box mt={5}>
+                    <Copyright />
+                  </Box>
+                </form>
+              </div>
+            </Grid>
+          </Grid>
+      </ThemeProvider>
     );
   }
 }
