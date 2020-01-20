10e6d259cd105ef9a22c3d1883a897c6a1938de6
JoaoCoelho8 <33787@ufp.edu.pt>
2020-01-20 23:47:55 +0000

    cors error

--- react/react-app/src/Components/EditRemove/EditRemoveContent.js
+++ react/react-app/src/Components/EditRemove/EditRemoveContent.js
@@ -1,77 +1,132 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { Table, Divider, Tag } from 'antd';
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <a>{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-      <span>
-        <a>Edit</a>
-        <Divider type="vertical" />
-        <a>Delete</a>
-      </span>
-    ),
-  },
-];
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-  },
-];
-
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
+import { Typography } from '@material-ui/core';
+import axios from 'axios'; 
 
 class EditRemoveContent extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-        page: 0,
-        rowsPerPage: 10,
-    };
-    this.handleChangePage=this.handleChangePage.bind(this);
-    this.handleChangeRowsPerPage=this.handleChangeRowsPerPage.bind(this);
-}
+      page: 0,
+      rowsPerPage: 10,
+      columnsRecipes: [
+        {
+          title: 'Recipe Name',
+          dataIndex: 'recipe_name',
+          key: 'name',
+        },
+        /*{
+          title: 'Description',
+          dataIndex: 'recipe_description',
+          key: 'recipe_description',
+        },*/
+        {
+          title: 'Duration',
+          dataIndex: 'duration',
+          key: 'duration',
+        },
+        {
+          title: 'Category',
+          dataIndex: 'category',
+          key: 'category',
+        },
+        {
+          title: 'Actions',
+          key: 'actions',
+          render: (text, record) => (
+            <span>
+              <a onClick={() => this.handleEditRecipeName(record.recipe_id)}>Edit</a>
+              <Divider type="vertical" />
+              <a onClick={() => this.handleDeleteRecipe(record.recipe_id)}>Delete</a> 
+            </span>
+          ),
+        },
+      ],
+      dataRecipes: [],
+      columnsIngredients: [
+          {
+            title: 'Ingredient Name',
+            dataIndex: 'ingredient_name',
+            key: 'ingredient_name',
+          },
+          {
+            title: 'Calories',
+            dataIndex: 'kcal',
+            key: 'kcal',
+          },
+          {
+            title: 'Actions',
+            key: 'actions',
+            render: (text, record) => (
+              <span>
+                <a onClick={() => this.handleEditIngredientName(record.ingredient_id)}>Edit</a>
+                <Divider type="vertical" />
+                <a onClick={() => this.handleDeleteIngredient(record.ingredient_id)}>Delete</a> 
+              </span>
+            ),
+          },
+        ],
+        dataIngredients: [],
+      };
+      this.handleChangePage=this.handleChangePage.bind(this);
+      this.handleChangeRowsPerPage=this.handleChangeRowsPerPage.bind(this);
+      this.handleDeleteRecipe=this.handleDeleteRecipe.bind(this);
+      this.handleDeleteIngredient=this.handleDeleteIngredient.bind(this);
+      this.handleEditRecipeName=this.handleEditRecipeName.bind(this);
+      this.handleEditIngredientName=this.handleEditIngredientName.bind(this);
+    }
 
+    componentDidMount() {
+      axios.get("http://localhost:8000/api/getIngredientByUserIdRoute/id/"+sessionStorage.getItem('access_token'))
+      .then(resulti => {
+          if (resulti.status==200) { 
+              this.setState({dataIngredients: resulti.data});
+          }
+      })
+
+      axios.get("http://localhost:8000/api/searchUserRecipe/id/"+sessionStorage.getItem('access_token'))
+      .then(resulti => {
+          if (resulti.status==200) { 
+              this.setState({dataRecipes: resulti.data});
+          }
+      })
+    }
+
+    //r.HandleFunc("/api/deleteRecipe/id/{id}", deleteRecipeRoute).Methods("DELETE")
+    handleDeleteRecipe = key => {
+      axios.delete('http://localhost:8000/api/deleteRecipe/id/'+key)
+      .then(resulti => {
+          console.log(resulti);
+          const dataRecipes = [...this.state.dataRecipes];
+          this.setState({ dataRecipes: dataRecipes.filter(item => item.key !== key) });
+      })
+    }
+
+    //r.HandleFunc("/api/editRecipeName", editRecipeNameRoute).Methods("POST")
+    handleEditRecipeName = key => {
+      alert("Edit Recipe not done yet!");
+    }
+
+    //r.HandleFunc("/api/deleteIngredient/id/{id}", deleteIngredientRoute).Methods("DELETE")
+    handleDeleteIngredient = key => {
+      axios.delete('http://localhost:8000/api/deleteIngredient/id/'+key)
+      .then(resulti => {
+          console.log(resulti);
+          const dataIngredients = [...this.state.dataIngredients];
+          this.setState({ dataIngredients: dataIngredients.filter(item => item.key !== key) });
+      })
+      
+    }
+
+    //r.HandleFunc("/api/editIngredientName", editIngredientNameRoute).Methods("POST")
+    handleEditIngredientName = key => {
+      alert("Edit Ingredient not done yet");
+    }
 
     handleChangePage = (event, newPage) => {
         this.setState({page: newPage})
@@ -85,9 +140,17 @@ class EditRemoveContent extends Component {
     render() {
     const { classes } = this.props;
 
+    if (sessionStorage.getItem('access_token') < 0) {
+      return <Redirect to='/signin'/>
+    }
+
     return (
         <Paper>
-          <Table columns={columns} dataSource={data} />
+          <Typography style={{textAlign: 'left', fontSize: 40}}>Recipes:</Typography>
+          <Table columns={this.state.columnsRecipes} dataSource={this.state.dataRecipes} />
+
+          <Typography style={{textAlign: 'left', fontSize: 40, paddingTop: '20'}}>Ingredients:</Typography>
+          <Table columns={this.state.columnsIngredients} dataSource={this.state.dataIngredients} />
         </Paper>
       );
     }
