f0825c49c2ade6377d269f022045b244c5a8667a
JoaoCoelho8 <33787@ufp.edu.pt>
2020-01-20 21:46:13 +0000

    fixed kcal error

--- react/react-app/src/Components/Insert/InsertHeader.js
+++ react/react-app/src/Components/Insert/InsertHeader.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import { Link } from 'react-router-dom';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import logo from '../images/image.png';
 
